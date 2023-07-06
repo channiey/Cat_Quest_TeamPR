5aecc1db5638ede2795f3c6a9a4719ca3625a98e
kkj6648 <kkj6648@naver.com>
2023-07-06 23:10:22 +0900

    Feat : "UI 클래스 생성"

--- Client/Code/UI.cpp
+++ Client/Code/UI.cpp
@@ -1,1 +1,91 @@
 #include "UI.h"
+#include "Export_Function.h"
+
+
+
+CUI::CUI(LPDIRECT3DDEVICE9 pGraphicDev)
+	: Engine::CGameObject(pGraphicDev, OBJ_TYPE::UI)
+	, m_pTextureCom(nullptr)
+	, m_eUItype(UITYPE_END)
+{
+}
+
+CUI::CUI(const CUI& rhs)
+	:Engine::CGameObject(rhs)
+{
+
+}
+
+CUI::~CUI()
+{
+}
+
+HRESULT CUI::Ready_Object()
+{
+	CGameObject::Ready_Object();
+	FAILED_CHECK_RETURN(Add_Component(), E_FAIL);
+
+	return S_OK;
+}
+
+_int CUI::Update_Object(const _float& fTimeDelta)
+{
+	__super::Update_Object(fTimeDelta);
+
+	return 0;
+}
+
+void CUI::LateUpdate_Object()
+{
+	__super::LateUpdate_Object();
+}
+
+void CUI::Render_Object()
+{
+
+
+	m_pTextureCom->Render_Texture();
+	m_pBufferCom->Render_Buffer();
+
+
+}
+
+HRESULT CUI::Add_Component()
+{
+	CComponent* pComponent = nullptr;
+
+
+	// Texture
+	pComponent = m_pTextureCom = dynamic_cast<CTexture*>(Engine::Clone_Proto(COMPONENT_TYPE::TEXTURE, this));
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	m_mapComponent[ID_STATIC].emplace(COMPONENT_TYPE::TEXTURE, pComponent);
+
+
+	// RcTex
+	pComponent = m_pBufferCom = dynamic_cast<CRcTex*>(Engine::Clone_Proto(COMPONENT_TYPE::BUFFER_RC_TEX, this));
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	m_mapComponent[ID_STATIC].emplace(COMPONENT_TYPE::BUFFER_RC_TEX, pComponent);
+
+	return S_OK;
+}
+
+CUI* CUI::Create(LPDIRECT3DDEVICE9 pGraphicDev)
+{
+
+	CUI* pInstance = new CUI(pGraphicDev);
+
+	if (FAILED(pInstance->Ready_Object()))
+	{
+		Safe_Release(pInstance);
+
+		MSG_BOX("UI Create Failed");
+		return nullptr;
+	}
+
+	return pInstance;
+}
+
+void CUI::Free()
+{
+	__super::Free();
+}
--- Client/Header/UI.h
+++ Client/Header/UI.h
@@ -1,6 +1,47 @@
 #pragma once
+#include "GameObject.h"
+#include "Engine_Define.h"
 
-class CUI
+BEGIN(Engine)
+
+class CTexture;
+class CRcTex;
+class CTransform;
+
+END
+
+class CUI : public Engine::CGameObject
 {
+
+protected:
+
+	explicit CUI(LPDIRECT3DDEVICE9 pGraphicDev);
+	explicit CUI(const CUI& rhs);
+	virtual ~CUI();
+
+	virtual HRESULT			Ready_Object() override;
+	virtual _int			Update_Object(const _float& fTimeDelta) override;
+	virtual void			LateUpdate_Object() override;
+	virtual void			Render_Object() override;
+
+
+
+private:
+	HRESULT					Add_Component();
+
+
+protected:
+	CTexture*				m_pTextureCom = nullptr;
+	UITYPE					m_eUItype;
+
+
+public:
+	static CUI*				Create(LPDIRECT3DDEVICE9 pGraphicDev);
+	
+
+protected:
+	virtual void			Free() override;
+
+	
 
 };
