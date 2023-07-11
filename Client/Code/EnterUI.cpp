27e9f82fe1bafa1142f0fc450080a223e72edc16
Juns <gmlwnsgmlcks@gmail.com>
2023-07-11 22:34:40 +0900

    Feat : "EnterUI 수정"

--- Client/Code/EnterUI.cpp
+++ Client/Code/EnterUI.cpp
@@ -18,17 +18,16 @@ CEnterUI::~CEnterUI()
 
 HRESULT CEnterUI::Ready_Object()
 {
-	m_eUIType = UI_TYPE::VIEW;
+	m_eUIType = UI_TYPE::WORLD;
 
 	D3DXMatrixIdentity(&m_UImatWorld);
 
-	m_fSizeX = 0.f;
-	m_fSizeY = 10.f;
-
-	m_UImatWorld._11 = m_fSizeX;
-	m_UImatWorld._22 = m_fSizeY;
-
 	FAILED_CHECK_RETURN(Add_Component(), E_FAIL);
+
+	m_fSizeX = 0.f;
+	m_fSizeY = 0.f;
+
+	m_pTransformCom->Set_Scale(_vec3{ m_fSizeX, m_fSizeY, 0.f });
 
 	return S_OK;
 }
@@ -41,10 +40,10 @@ _int CEnterUI::Update_Object(const _float& fTimeDelta)
 
 		if (m_bIsStart)
 		{
-			m_fSizeX += 100.f * fTimeDelta;
-			m_fSizeY += 100.f * fTimeDelta;
+			m_fSizeX += 10.f * fTimeDelta;
+			m_fSizeY += 10.f * fTimeDelta;
 
-			if (m_fSizeX >= 55)
+			if (m_fSizeX >= 3.5)
 			{
 				m_bIsShirk = true;
 				m_bIsStart = false;
@@ -54,29 +53,27 @@ _int CEnterUI::Update_Object(const _float& fTimeDelta)
 
 		if (m_bIsEnd)
 		{
-			m_fSizeX -= 100.f * fTimeDelta;
-			m_fSizeY -= 115.f * fTimeDelta;
+			m_fSizeX -= 10.f * fTimeDelta;
+			m_fSizeY -= 10.f * fTimeDelta;
 
 			if (m_fSizeX <= 0)
 			{
 				m_fSizeX = 0.f;
-				m_fSizeY = 12.f;
-
-				m_UImatWorld._11 = m_fSizeX;
-				m_UImatWorld._22 = m_fSizeY;
+				m_fSizeY = 0.f;
 
 				m_bIsOn = false;
 				m_bIsEnd = false;
+
 			}
 		}
 
 
 		if (m_bIsExpand)
 		{
-			m_fSizeX += 16.f * fTimeDelta;
-			m_fSizeY += 16.f * fTimeDelta;
+			m_fSizeX += 2.5f * fTimeDelta;
+			m_fSizeY += 2.5f * fTimeDelta;
 
-			if (55 < m_fSizeX)
+			if ( 3.5f < m_fSizeX)
 			{
 				m_bIsExpand = false;
 				m_bIsShirk = true;
@@ -85,10 +82,10 @@ _int CEnterUI::Update_Object(const _float& fTimeDelta)
 
 		if (m_bIsShirk)
 		{
-			m_fSizeX -= 30.f * fTimeDelta;
-			m_fSizeY -= 30.f * fTimeDelta;
+			m_fSizeX -= 3.5f * fTimeDelta;
+			m_fSizeY -= 3.5f * fTimeDelta;
 
-			if (45 > m_fSizeX)
+			if ( 2.5f > m_fSizeX)
 			{
 				m_bIsExpand = true;
 				m_bIsShirk = false;
@@ -106,8 +103,7 @@ void CEnterUI::LateUpdate_Object()
 {
 	if (m_bIsOn)
 	{
-		m_UImatWorld._11 = m_fSizeX;
-		m_UImatWorld._22 = m_fSizeY;
+		m_pTransformCom->Set_Scale(_vec3{ m_fSizeX, m_fSizeY, 0.f });
 
 		__super::LateUpdate_Object();
 	}
@@ -119,7 +115,7 @@ void CEnterUI::Render_Object()
 	if (m_bIsOn)
 	{
 		m_pGraphicDev->SetMaterial(&material.Get_Meretial(color.white));
-		m_pGraphicDev->SetTransform(D3DTS_WORLD, &m_UImatWorld);
+		m_pGraphicDev->SetTransform(D3DTS_WORLD, &m_pTransformCom->Get_WorldMat());
 
 		m_pTextureCom->Render_Texture(_uint(m_eUIEnter));
 		m_pBufferCom->Render_Buffer();
@@ -129,7 +125,7 @@ void CEnterUI::Render_Object()
 
 void CEnterUI::EnterUI_On(UIENTER_TYPE eUIEnter, CGameObject* pObj)
 {
-	if (m_bIsOn)
+	if (true == m_bIsOn)
 		return;
 
 	m_bIsOn = true;
@@ -137,26 +133,17 @@ void CEnterUI::EnterUI_On(UIENTER_TYPE eUIEnter, CGameObject* pObj)
 	m_eUIEnter = eUIEnter;
 
 	_vec3 vObjWorldPos = pObj->Get_Transform()->Get_Info(INFO::INFO_POS);
-	_matrix matWorld = pObj->Get_Transform()->Get_WorldMat();
-	_matrix matView = CCameraMgr::GetInstance()->Get_CurCamera()->Get_CameraCom()->Get_MatView();
-	_matrix matProj = CCameraMgr::GetInstance()->Get_CurCamera()->Get_CameraCom()->Get_MatProj();
-	D3DVIEWPORT9 pViewport = CCameraMgr::GetInstance()->Get_CurCamera()->Get_CameraCom()->Get_ViewPort();
 
-	D3DXVec3TransformCoord(&vObjWorldPos, &vObjWorldPos, &matView);
-	D3DXVec3TransformCoord(&vObjWorldPos, &vObjWorldPos, &matProj);
+	vObjWorldPos.y += 4;
 
-	_float ScreenX = vObjWorldPos.x * (pViewport.Width / 2) + pViewport.X + (pViewport.Width / 2);
-	_float ScreenY = -vObjWorldPos.y * (pViewport.Height / 2) + pViewport.Y + (pViewport.Height / 2);
 
-	m_UImatWorld._41 = ScreenX;
-	m_UImatWorld._42 = ScreenY + 70;
+	m_pTransformCom->Set_Pos(vObjWorldPos);
+
 }
 
 void CEnterUI::EnterUI_Off()
 {
-	if (m_bIsStart)
-		return;
-
+	m_bIsStart = false;
 	m_bIsExpand = false;
 	m_bIsShirk = false;
 	m_bIsEnd = true;
@@ -174,6 +161,10 @@ HRESULT CEnterUI::Add_Component()
 	pComponent = m_pBufferCom = dynamic_cast<CRcTex*>(Engine::Clone_Proto(COMPONENT_TYPE::BUFFER_RC_TEX, this));
 	NULL_CHECK_RETURN(pComponent, E_FAIL);
 	m_mapComponent[ID_STATIC].emplace(COMPONENT_TYPE::BUFFER_RC_TEX, pComponent);
+
+	pComponent = m_pTransformCom = dynamic_cast<CTransform*>(Engine::Clone_Proto(COMPONENT_TYPE::TRANSFORM, this));
+	NULL_CHECK_RETURN(pComponent, E_FAIL);
+	m_mapComponent[ID_STATIC].emplace(COMPONENT_TYPE::TRANSFORM, pComponent);
 
 
 	return S_OK;
---
+++ Client/Code/IndicatorUI.cpp
@@ -0,0 +1,1 @@
+#include "IndicatorUI.h"
