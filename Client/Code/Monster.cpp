1c6ecdf39aea80cd467223b5af35655b92f4cdbd
kang2x2 <kang2x2@naver.com>
2023-07-17 22:11:24 +0900

    Fix : "몬스터 기본공격 피격 이펙트 한 번만 뜨도록 수정"

--- Client/Code/Monster.cpp
+++ Client/Code/Monster.cpp
@@ -125,8 +125,6 @@ Engine::_int CMonster::Update_Object(const _float& fTimeDelta)
 	{
 		fAccTime += fTimeDelta;
 
-		CEventMgr::GetInstance()->Add_Obj(L"Monster_Cut_Effect", CMobCutEffect::Create(m_pGraphicDev, m_pTransformCom->Get_Info(INFO_POS)));
-
 		if (fAccTime >= 0.2f) // 플레이어 딜레이 만큼이 베스트
 		{
 			m_bHit = false;
@@ -243,7 +241,8 @@ void CMonster::Damaged(CGameObject* pObj)
 		m_pRigidBodyCom->Zero_KnockBack();
 	}
 	m_pRigidBodyCom->Knock_Back(pObj, 220);
-	
+	CEventMgr::GetInstance()->Add_Obj(L"Monster_Cut_Effect", CMobCutEffect::Create(m_pGraphicDev, m_pTransformCom->Get_Info(INFO_POS)));
+
 
 }
 
