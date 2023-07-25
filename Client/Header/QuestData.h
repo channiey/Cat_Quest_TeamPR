d5ea39eaaffffb90ecdd0d53f8bc9f751d151c6c
kang2x2 <kang2x2@naver.com>
2023-07-26 02:10:21 +0900

    Doc : "퀘스트 제목, 내용 담을 변수 추가"

--- Client/Header/QuestData.h
+++ Client/Header/QuestData.h
@@ -24,14 +24,18 @@ public:
 public:
 	virtual _int Get_Level() { return m_iLevel; }
 	virtual void Next_Level() { m_iLevel += 1; }
+
+	virtual wstring Get_QuestName() { return m_strQuestName; }
+	virtual wstring Get_QuestContent() { return m_strQuestContent; }
 protected:
 	wstring					m_strQuestName; // 퀘스트 이름
+	wstring					m_strQuestContent; // 퀘스트 내용
 	vector<CGameObject*>	m_vNpcList;		// 관련 Npc를 담을 배열
 	vector<CGameObject*>	m_vItemList;	// 보상 아이템을 담을 배열
 	vector<CSkill*>			m_vSkillList;	// 보상 스킬을 담을 배열
 	_int					m_iLevel;		// 퀘스트 단계
 	CGameObject*			m_pPlayer;      // 플레이어
-
+	
 
 private:
 	virtual void			Free();
