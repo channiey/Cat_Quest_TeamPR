f99466d99c2b5c2c794b41d0a37ddcc2dd1c447d
kkj6648 <kkj6648@naver.com>
2023-07-30 20:27:32 +0900

    Doc : "보스 테스트용 제거"

--- Client/Code/VioletDragon.cpp
+++ Client/Code/VioletDragon.cpp
@@ -97,7 +97,7 @@ HRESULT CVioletDragon::Ready_Object()
 
 	// Stat Info
 	m_tStatInfo.bDead = false;
-	m_tStatInfo.fCurHP = 200.f;
+	m_tStatInfo.fCurHP = 3000.f;
 	m_tStatInfo.fMaxHP = m_tStatInfo.fCurHP;
 	m_tStatInfo.fAD = 10.f;
 
