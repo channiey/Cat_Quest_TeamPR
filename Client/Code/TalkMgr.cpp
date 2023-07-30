c4da0e208c3714586c74f32ba8b4427947b3d4c5
Chan Yoon <psalms119165sin@gmail.com>
2023-07-30 20:19:08 +0900

    Doc : "겟 이펙트 보간 수정"

--- Client/Code/TalkMgr.cpp
+++ Client/Code/TalkMgr.cpp
@@ -241,7 +241,7 @@ _bool CTalkMgr::Get_CamTalk(LPDIRECT3DDEVICE9 pGraphicDev,
 			{
 				m_pCam = dynamic_cast<CPlayer_Camera*>(CCameraMgr::GetInstance()->Get_CurCamera());
 				CCameraMgr::GetInstance()->Start_Action(CAMERA_ACTION::OBJ_CHANGE_TARGET, _StartPos, _TargetPos, TRUE);
-				CSoundMgr::GetInstance()->PlaySoundW(L"map_transition.wav", CHANNEL_ID::UI_3, VOLUME_PLAYER_UI);
+				CSoundMgr::GetInstance()->PlaySoundW(L"map_transition.wav", CHANNEL_ID::UI_3, 1.f);
 
 				m_bTargetCam = true;
 			}
@@ -299,7 +299,7 @@ _bool CTalkMgr::Get_CamTalk(LPDIRECT3DDEVICE9 pGraphicDev,
 						{
 							CCameraMgr::GetInstance()->Start_Action(CAMERA_ACTION::OBJ_CHANGE_TARGET, _TargetPos, _StartPos, FALSE);
 							m_pCam = dynamic_cast<CPlayer_Camera*>(CCameraMgr::GetInstance()->Get_CurCamera());
-							CSoundMgr::GetInstance()->PlaySoundW(L"map_transition.wav", CHANNEL_ID::UI_3, VOLUME_PLAYER_UI);
+							CSoundMgr::GetInstance()->PlaySoundW(L"map_transition.wav", CHANNEL_ID::UI_3, 1.f);
 
 							m_bReturnCam = true; 
 						}
